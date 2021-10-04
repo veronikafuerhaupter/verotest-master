b50173440dc5652724e5efa186936325c1e40adf
verofuerhaupter <fuerhaup@hm.edu>
2021-10-04 14:19:28 +0200

    Changes 15.09.

--- src/verotest/ros/imagehandler.py
+++ src/verotest/ros/imagehandler.py
@@ -28,6 +28,7 @@ class Imagehandler:
     reference_entry = None
     match_list = None
     cropped_list = None
+    ratio_circles = None
 
     def __init__(self):
         self.search_list = []
@@ -57,12 +58,12 @@ class Imagehandler:
 
     def crop_perm_area(self, found, img):
         """
-        This method crops the colored images. Later on, the final net will need to identify the position of the Springmittel Minus.
-        Therefore, it is necessary to eliminate part of the picture where the position of the Springmittel in the pallet cannot be identified anymore.
-        At the same time depth image will be cropped according to the color image. The dimensions of the image were taken by eye for reasons of practicality.
-        As soon as it is no longer possible for the human eye to recognise how a Springmittel is arranged, the pallet must be shifted.
+        This method crops the colored images. Later on, the final net will need to identify the position of the
+        Springmittel Minus.Therefore, it is necessary to eliminate part of the picture where the position of the
+        Springmittel in the pallet cannot be identified anymore.At the same time depth image will be cropped according
+        to the color image. The dimensions of the image were taken by eye for reasons of practicality.As soon as it is
+        no longer possible for the human eye to recognise how a Springmittel is arranged, the pallet must be shifted.
         """
-
         perm_width_left = 310
         perm_width_right = 1060
         perm_height_top = 40
@@ -83,7 +84,9 @@ class Imagehandler:
         return color_cropped, depth_cropped, color_width, color_height
 
     def predict_pallet(self, color_cropped):
-
+        """
+        This method predicts the pallet from an Azure Custom Vision model
+        """
         with open(PALLET_LABELS_FILENAME, 'r') as f:
             labels = [l.strip() for l in f.readlines()]
 
@@ -93,20 +96,23 @@ class Imagehandler:
         return predictions
 
     def crop_pallet(self, predictions, color_width, color_height, color_cropped, depth_cropped):
-
+        """
+        This method crops the predicted pallet in its dimensions
+        """
         pallet_depth_cropped = None
         pallet_color_cropped = None
         pallet_color_height = None
         pallet_color_width = None
 
         for i in range(0, len(predictions)):
-            '''if len(predictions) == 0:
+            """
+            if len(predictions) == 0:
                 return print("There was no pallet detected")
 
             #The processing of the image is interrupted when the pallet cannot be properly detected, threshold is 0.8
             elif predictions[i]['probability'] < 0.6:
-                return print("Pallet cannot be properly detected")'''
-
+                return print("Pallet cannot be properly detected")
+            """
             #elif predictions[i]['probability'] > 0.6:
             left = int(predictions[i]['boundingBox']['left'] * color_width)
             top = int(predictions[i]['boundingBox']['top'] * color_height)
@@ -126,8 +132,38 @@ class Imagehandler:
 
             return pallet_depth_cropped, pallet_color_cropped, pallet_color_height, pallet_color_width
 
-    def predict_springmittel(self, pallet_color_cropped):
+    def predict_circles(self, pallet_color_cropped):
+        """
+        This method uses the Hough Circle Transformation for detecting the inner circle within the Springmittel.
+        The maximum radius of 20 pixels ensures that the inner circle is detected, whereas the minimum distance of 300 pixels
+        to other circles ensures that only one circle on the cropped pallet is detected.
+        """
+        gray = cv2.cvtColor(pallet_color_cropped, cv2.COLOR_BGR2GRAY)
+        circles = cv2.HoughCircles(gray, cv2.HOUGH_GRADIENT, dp=3, minDist=300, param1=600, param2=20, minRadius=12, maxRadius=14)
 
+        return circles
+
+    def crop_springmittel_circles(self, circles, pallet_color_cropped, pallet_depth_cropped):
+        """
+        The Hough Circle Transformation gives information about the radius in pixels.
+        As a result the method converts the dimensions of the Springmittel into pixels and
+        crops the inner circle around the Springmittel around the hole in the middle
+          """
+        ratio_circles = circles[0][0][2] / 8
+        left = int(circles[0][0][0] - 28 * ratio_circles)
+        right = int(circles[0][0][0] + 28 * ratio_circles)
+        top = int(circles[0][0][1] - 28 * ratio_circles)
+        bottom = int(circles[0][0][1] + 28 * ratio_circles)
+
+        springmittel_color_cropped = pallet_color_cropped[top:bottom, left:right]
+        springmittel_depth_cropped = pallet_depth_cropped[int(top / 2):int(bottom / 2), int(left / 2):int(right / 2)]
+
+        return springmittel_color_cropped, springmittel_depth_cropped
+
+    def predict_springmittel(self, pallet_color_cropped):
+        """
+        Predict the Springmittel from Azure Custom Vision model
+        """
         with open(SPRINGMITTEL_LABELS_FILENAME, 'r') as f:
             labels = [l.strip() for l in f.readlines()]
 
@@ -135,30 +171,6 @@ class Imagehandler:
         predictions_springmittel = od_model.predict_image(pallet_color_cropped)
 
         return predictions_springmittel
-
-    def crop_springmittel(self, predictions_springmittel, pallet_color_width, pallet_color_height, pallet_depth_cropped, pallet_color_cropped):
-
-        springmittel_color_cropped = None
-        springmittel_depth_cropped = None
-
-        for i in range(0, len(predictions_springmittel)):
-            '''if len(predictions_springmittel) == 0:
-                print("There was no springmittel detected")
-
-            elif predictions_springmittel[i]['probability'] < 0.3:
-                print("Springmittel cannot be properly detected")
-
-            elif predictions_springmittel[i]['probability'] > 0.3:'''
-            left = int(predictions_springmittel[i]['boundingBox']['left'] * pallet_color_width)
-            top = int(predictions_springmittel[i]['boundingBox']['top'] * pallet_color_height)
-            right = int(left + predictions_springmittel[i]['boundingBox']['width'] * pallet_color_width)
-            bottom = int(top + predictions_springmittel[i]['boundingBox']['height'] * pallet_color_height)
-
-            springmittel_color_cropped = pallet_color_cropped[top:bottom, left:right]
-            springmittel_depth_cropped = pallet_depth_cropped[int(top / 2):int(bottom / 2), int(left / 2):int(right/2)]
-            print('Springmittel cropped')
-
-            return springmittel_color_cropped, springmittel_depth_cropped
 
     def handle_cropped_img(self, springmittel_color_cropped, springmittel_depth_cropped):
 
@@ -170,6 +182,9 @@ class Imagehandler:
         return self.cropped_list
 
     def find_entry_temporal_closest_to(self, reference_entry, search_list, threshold=0.5):
+        #timestamp liste händisches Label
+        #drücken: nächster depth frame, pro Serienaufnahme 8 matches
+
         """
         This method searches the passed list for an entry which time attribute is closest to the
         time of the reference entry and the distance between reference time and the entry time
